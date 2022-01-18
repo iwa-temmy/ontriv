ade65be8b5788482d903e5e1c2c20ef98811fb4b
Gracetemitope <gracetemitope14@gmail.com>
2022-01-18 10:13:36 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Router from './Router'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App() {
+  return (
+     <Router/>
+  );
+}
+
+export default App;
---
+++ src/Router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter } from "react-router-dom"
+
+
+const RouterConfig = () => {
+    return (
+        <BrowserRouter>
+                <h1>Ontriv</h1>
+        </BrowserRouter >
+    )
+
+}
+export default RouterConfig
