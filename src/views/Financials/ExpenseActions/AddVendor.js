b78d13e9bebda44b2b60871f7d367848d6670fc7
Olawalecodes <iwaloyeo@gmail.com>
2022-08-10 21:17:16 +0100

    Add vendor modal in view

--- src/views/Financials/ExpenseActions/AddVendor.js
+++ src/views/Financials/ExpenseActions/AddVendor.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Input } from "reactstrap";
+import { Input, Row, Col } from "reactstrap";
 import XCancel from "../../../assets/img/x-cancel.svg";
 
-const AddVendor = ({setShowVendor}) => {
+const AddVendor = ({ setShowVendor }) => {
   return (
     <div className="off-canvas-menu">
-      <div className="off-canvas-menu__content px-5 py-2">
+      <div className="off-canvas-menu__content py-2">
         <div className="d-inline-flex w-100">
           <div className="add-client-text text-center">
             <h5>Add new vendor</h5>
@@ -17,36 +17,40 @@ const AddVendor = ({setShowVendor}) => {
             alt=""
           />
         </div>
+        <Row className="px-4">
+          <Col sm="12" md="12" lg="12" xl="12">
+            <label className="text-left w-100">Vendor Name</label>
+            <Input
+              type="text"
+              placeholder=""
+              className="off-canvas-menu__input py-3 px-3"
+            />
+          </Col>
+          <Col sm="12" md="12" lg="12" xl="12">
+            <label className="text-left w-100">Email Address</label>
+            <Input
+              type="email"
+              placeholder=""
+              className="off-canvas-menu__input py-3 px-3"
+            />
+          </Col>
+          <Col>
+            <label className="text-left w-100 mt-4">Phone Number</label>
+            <Input
+              type="tel"
+              placeholder=""
+              className="off-canvas-menu__input py-3 px-3"
+            />
+          </Col>
+        </Row>
 
-        <label className="text-left w-100">Vendor Name</label>
-        <Input
-          type="text"
-          placeholder=""
-          className="off-canvas-menu__input py-3 px-3"
-        />
-        <label className="text-left w-100">Email Address</label>
-        <Input
-          type="email"
-          placeholder=""
-          className="off-canvas-menu__input py-3 px-3"
-        />
-        <label className="text-left w-100 mt-4">Phone Number</label>
-        <Input
-          type="tel"
-          placeholder=""
-          className="off-canvas-menu__input py-3 px-3"
-        />
-        <div>
-          <label className="text-left w-100 mt-4">Address</label>
-          <textarea
-            rows="4"
-            className="w-100 rounded-3 mt-2 canvas-textarea"
-          ></textarea>
-        </div>
-        <div className="d-inline-flex mt-4 w-100 ">
-          <button className="py-3 ms-3 ms-auto px-4 send align-items-center ">
-            Add Vendor
-          </button>
+        <div className="position-absolute bottom-0 w-100">
+          <hr />
+          <div className="d-flex justify-content-end mt-2 w-100 mb-4 ">
+            <button className="py-3 px-4 send" style={{ marginRight: "18px" }}>
+              Add Expense
+            </button>
+          </div>
         </div>
       </div>
     </div>
